ac4bff16d97c0aaa146602cb62453e18457c89d1
ekal <macbookpro2017@Macbooks-MacBook-Pro.local>
2025-05-18 13:12:45 +0700

    gaskan hamszz

--- src/components/DashboardSidebar.jsx
+++ src/components/DashboardSidebar.jsx
@@ -9,6 +9,7 @@ const DashboardSidebar = ({
     toggleSidebar,
     handleLogout,
     navigateToKelolaKegiatan,
+    navigateToSeePresensi,
     navigateToKelolaTugas,
     navigateToAlQuran,
     navigateToPresensi,
@@ -119,7 +120,21 @@ const DashboardSidebar = ({
                 navigateToKelolaTugas();
                 closeSidebar && closeSidebar();
             },
-            roles: ['3', '4'] 
+            roles: ['3', '4']
+        },
+        {
+            id: 'lihat-presensi',
+            icon: (
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
+                </svg>
+            ),
+            label: "Lihat Presensi",
+            onClick: () => {
+                navigateToSeePresensi();
+                closeSidebar && closeSidebar();
+            },
+            roles: ['3', '4']
         },
         {
             id: 'kegiatan',
